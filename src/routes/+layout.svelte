b31a13ec07a453648444ab93c23c4b71deb02be3
J-boardman <jaydeboardman@outlook.com>
2024-01-02 20:15:20 +1000

    Speed Insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 	import Header from '$lib/components/ui/Header.svelte';
 	import { onNavigate } from '$app/navigation';
 	import Background from '$lib/components/ui/Background.svelte';
-	import MobileNav from '$lib/components/ui/MobileNav.svelte';
-	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+	injectSpeedInsights();
 	onNavigate((navigation) => {
 		// @ts-expect-error
 		if (!document?.startViewTransition) return;
